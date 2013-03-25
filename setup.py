49b4f627463234e4c250fceaeb9fab52b69cc7d5
Matt Austin <matt.austin@thecut.net.au>
2013-03-25 10:06:07 +0800

    Updated required version of ``django-model-utils``, and ``django-countries``.

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 from version import get_git_version
 
 
-setup(name='thecut-contacts',
-    author='The Cut', author_email='development@thecut.net.au',
+setup(
+    name='thecut-contacts',
+    author='The Cut',
+    author_email='development@thecut.net.au',
     url='http://projects.thecut.net.au/projects/thecut-contacts',
     namespace_packages=['thecut'],
     version=get_git_version(),
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['distribute', 'django-countries==1.4',
-        'django-model-utils==1.1.0', 'django-tagging==0.3.1'],
+    install_requires=['distribute', 'django-countries>=1.5',
+                      'django-model-utils>=1.2.0', 'django-tagging==0.3.1'],
     )
-
